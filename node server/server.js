95b99cac8331fe0830af3743014235cb88174b0f
Victor home <viktor-it@inbox.ru>
2018-09-26 06:31:51 +0400

    Изменил код, который указан в книге

--- node server/server.js
+++ node server/server.js
@@ -1,16 +1,15 @@
-var args = process.argv.slice(2); // проверим аргументы: путь к файлу и порт
-if (args.length != 2) {
-	console.log('Не достаточно аргументов');
-	return;
-	}
-// проверим наличие файла
-var fs = require('fs'); if (!fs.existsSync(args[0])){
- console.log('Файл ' + args[0] + ' не найден');
- return;
- }
- // запускаем сервер
-var http = require('http');
-http.createServer( function(req, res) { res.writeHead(200, {'Content-Type': 'text/html'});
-res.end(fs.readFileSync(args[0], {'encoding': 'utf-8'})); } ).listen(args[1], '127.0.0.1');
-console.log('Сервер успешно запущен, порт ' + args[1]);
+//загрузка модуля http
+let http = require ('http');
 
+//создание http-сервера
+http.createServer(function(req, res) {
+	//заголовок контента
+	res.writeHead(200, {'content-type':'text/plain'});
+
+	//запись сообщения и завершения сигнальной связи
+	res.write("Hellow,WORLD!\n");
+	res.end();
+
+}).listen(8124);
+
+console.log("Сервер запущен по порту - 8124");
